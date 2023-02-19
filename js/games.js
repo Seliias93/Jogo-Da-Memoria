01f7e83ce864b746a88e2c850d82fbb04cac7828
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-02-19 16:16:18 -0300

    ,

--- js/games.js
+++ js/games.js
@@ -1,21 +1,45 @@
 const grid = document.querySelector('.grid');
 
-const createCard = () => {
-  const card = document.createElement('div');
-  const front = document.createElement('div');
-  const back = document.createElement('div');
+const pesona =[
+  'Ajudante_de_papainoel1',
+  'bart',
+  'card01',
+  'homer e flandes',
+  'homer',
+  'lisa',
+  'maggie',
+  'marge',
+  'simpsons2',
+  '480-4806621_homero-simpson-wallpaper-4k-hd-png-download',
+];
 
-  card.className = 'card';
-  front.className = 'face front';
-  back.className = 'face back';
+const createElement = (tag, className) => {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+const createCard = (pesona) => {
+  const card = createElement('div', 'card');
+  const front = createElement('div', 'face front');
+  const back = createElement('div', 'face back');
+
+  front.style.backgroudImage = `url('../imagens/${pesona}.png')`;
 
   card.appendChild(front);
   card.appendChild(back);
 
-  grid.appendChild(card);
+ return card;
 
 }
 
-createCard();
+const loadgame = () =>{
+  pesona.forEach((pesona) => {
 
+    const card = createCard(pesona);
+    grid.appendChild(card);
+  });
 
+}
+
+loadgame();
