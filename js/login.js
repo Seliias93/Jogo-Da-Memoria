b61ef9cb701a2fa1b50f976fc20b596dabdc5d21
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2023-02-19 12:09:59 -0300

    .

--- js/login.js
+++ js/login.js
@@ -1,19 +1,19 @@
-const input = document.querySelector('.login_input');
-const button = document.querySelector('.login_button');
-const form = document.querySelector('.login_form');
+const input = document.querySelector(".login_input");
+const button = document.querySelector(".login_button");
+const form = document.querySelector(".login_form");
 
-const validateIput = ({ target }) =>{
-    if(target.value.length > 2){
-        button.removeAttribute('disabled'); 
-    }else{ 
-        button.setAttribute('disabled', '')
-    }
-}
-const  handlesubmit = (event) => {
-    event.preventDefault();
+const validateIput = ({ target }) => {
+  if (target.value.length > 2) {
+    button.removeAttribute("disabled");
+  } else {
+    button.setAttribute("disabled", "");
+  }
+};
+const handlesubmit = (event) => {
+  event.preventDefault();
 
-    localStorage.setItem('player', input.value);
-    window.location = 'pages/games.html';
-}
-input.addEventListener('input', validateIput);
-form.addEventListener('submit', handlesubmit);
+  localStorage.setItem("player", input.value);
+  window.location = "pages/games.html";
+};
+input.addEventListener("input", validateIput);
+form.addEventListener("submit", handlesubmit);
